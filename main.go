ab0f9e58cd8d622eb60bc57aeb081c6bfeba7524
jzayac <zayac123@gmail.com>
2016-10-19 03:45:27 +0200

    tab to spaces

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"time"
+  "fmt"
+  "math"
+  "time"
 )
 
 type position struct {
@@ -31,8 +31,8 @@ func (r queen) isDiagonal(q queen) bool {
 func abs(x int) int {
   ret := x;
   if (ret < 0) {
-	  ret *= -1
-	}
+    ret *= -1
+  }
   return ret
 }
 
@@ -41,64 +41,64 @@ var solution int
 
 func intInSlice(i int, list []int) bool {
   for _, b := range list {
-	  if b == i {
-		  return true
-		}
-	}
-	return false
+    if b == i {
+      return true
+    }
+  }
+  return false
 }
 
 func findNext(queens []queen, column []int) {
   if len(queens) == nQueen {
-	  _ = "breakpoint"
-	  solution++ 
-	} else {
-	  for i := 0; i < nQueen; i++ {
-		  row := len(queens)
+    _ = "breakpoint"
+    solution++ 
+  } else {
+    for i := 0; i < nQueen; i++ {
+      row := len(queens)
       current := queen{position{i, row}}
-			if (!conflict(queens, current, column)) {
-				findNext(append(queens, current), append(column, i))
-			}
-		}
-	}
+      if (!conflict(queens, current, column)) {
+        findNext(append(queens, current), append(column, i))
+      }
+    }
+  }
 }
 
 func conflict(queens []queen, current queen, column []int) bool {
   if (intInSlice(current.getColumn(), column)) {
-	  return true
-	}
-	for _, q := range queens {
+    return true
+  }
+  for _, q := range queens {
     if (current.isDiagonal(q)) {
-		  return true
-		}
-	}
-	return false
+      return true
+    }
+  }
+  return false
 }
 
 
 func start() {
-	for i := 0; i < int(math.Ceil(float64(nQueen)/2.0)); i++ {
+  for i := 0; i < int(math.Ceil(float64(nQueen)/2.0)); i++ {
     column := []int{i}
-		queens := make([]queen, 1, 12)
+    queens := make([]queen, 1, 12)
     queens[0] = queen{position{i, 0}}
-		findNext(queens, column)
+    findNext(queens, column)
 
     if (i != 0 && int(math.Floor(float64(nQueen)/2.0)) == i + 1) {
-		  solution *= 2
-		}
-	}
+      solution *= 2
+    }
+  }
 }
 
 func main() {
-	nQueen = 15
-	solution = 0
+  nQueen = 15
+  solution = 0
   startTime := time.Now().UnixNano() / int64(time.Millisecond)
 
   fmt.Printf("find: %d\n", nQueen)
 
-	start()
+  start()
   fmt.Printf("time: %d\n", (time.Now().UnixNano() / int64(time.Millisecond)  - startTime))
-	fmt.Printf("-----------------\n")
-	fmt.Printf("result: %d\n", solution)
-	fmt.Printf("-----------------\n")
+  fmt.Printf("-----------------\n")
+  fmt.Printf("result: %d\n", solution)
+  fmt.Printf("-----------------\n")
 }
